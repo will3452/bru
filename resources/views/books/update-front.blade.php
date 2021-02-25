954442dd73e114127fdfd2a417be4af79dcde118
unknown <williamgalas2@gmail.com>
2021-02-25 12:04:41 +0800

    fixed book 1

--- resources/views/books/update-front.blade.php
+++ resources/views/books/update-front.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.admin')
 @section('main-content')
     <h1>
-        Upload  .PDF file that contain your BOOK TITLE PAGE, COPYRIGHT PAGE, ACKNOWLEDGMENT PAGE AND DEDICATION PAGE
+        Upload 
     </h1>
     <div class="car card-body">
-       <form action="{{ route('books.update-front', $id) }}" enctype="multipart/form-data">
+       <form action="{{ route('books.update-front', $id) }}" method="POST" enctype="multipart/form-data">
             @method('PUT')
            @csrf
-        <div>
+        <div class="form-group">
             <input type="file" name="front" accept="application/pdf">
         </div>
         <button class="btn btn-primary">
