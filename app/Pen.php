822404c141134e3fc703a6ab11f523be4114b77e
unknown <williamgalas2@gmail.com>
2021-04-14 15:31:51 +0800

    fixed

--- app/Pen.php
+++ app/Pen.php
@@ -5,6 +5,7 @@ namespace App;
 use App\Art;
 use App\Book;
 use App\Song;
+use App\Audio;
 use App\Thrailer;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -16,6 +17,7 @@ class Pen extends Model
     
     public function canDelete(){
         if(Book::where('author',$this->name)->count()) return false;
+        if(Audio::where('author',$this->name)->count()) return false;
         if(Art::where('artist', $this->name)->count()) return false;
         if(Song::where('artist', $this->name)->orWhere('composer', $this->name)->orWhere('lyricist')->count()) return false;
         if(Thrailer::where('author', $this->name)->count()) return false;
