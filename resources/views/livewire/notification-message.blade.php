8bb75c59bb0fd184e02263c5d915ba33a7a81777
unknown <williamgalas2@gmail.com>
2021-04-22 06:13:09 +0800

    fixed

--- resources/views/livewire/notification-message.blade.php
+++ resources/views/livewire/notification-message.blade.php
@@ -1,6 +1,7 @@
 <li  class="nav-item dropdown no-arrow mx-1">
-    <a wire:poll class="nav-link dropdown-toggle" href="{{ route('inbox.index') }}" id="messagesDropdown"  >
+    <a wire:poll.750ms class="nav-link dropdown-toggle" href="{{ route('inbox.index') }}" id="messagesDropdown"  >
         <i class="fas fa-envelope fa-fw"></i>
+        {{ now() }}
         <!-- Counter - Messages -->
         @if (count(auth()->user()->unread_messages))
             <span class="badge badge-danger badge-counter">{{ count(auth()->user()->unread_messages) }}</span>
