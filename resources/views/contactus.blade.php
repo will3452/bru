f98e3c9cbdb7816eda9eced797a16360e80f8e3d
unknown <williamgalas2@gmail.com>
2021-06-14 07:29:24 +0800

    adding submissions@brumultiverse.com

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -94,7 +94,7 @@
             Help us make a difference by touching hearts and souls. 
         </section>
         <section>
-            For interested authors and artists, please send sample materials – short stories or first three chapters of a manuscript you’re working on or your visual works of art – to brumultiverse@gmail.com, along with a brief introduction about you and your artistic life. We will get back to you as soon as we can. 
+            For interested authors and artists, please send sample materials – short stories or first three chapters of a manuscript you’re working on or your visual works of art – to submissions@brumultiverse.com, along with a brief introduction about you and your artistic life. We will get back to you as soon as we can. 
         </section>
         <section>
             Please note, however, that while we recognize your awesome works and ideas, we only publish stories set within the BRUMULTIVERSE. 
