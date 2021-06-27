9e02139cab5c0db74944c394bd3af9cbfddf1479
unknown <williamgalas2@gmail.com>
2021-06-27 22:38:29 +0800

    fixed:

--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,7 @@ Route::prefix('v1')->group(function(){
         ]);
 
         // 
-        $testing = Testing::create($request->myname);
+        $testing = Testing::create(['myname'=>$request->name]);
         return response(['result'=>$testing], 201);
     });
     
