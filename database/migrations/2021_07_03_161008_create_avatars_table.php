b39a93a68147365ffeac04aa3ec948e0cf2e867f
unknown <williamgalas2@gmail.com>
2021-07-04 23:48:51 +0800

    register

--- database/migrations/2021_07_03_161008_create_avatars_table.php
+++ database/migrations/2021_07_03_161008_create_avatars_table.php
@@ -30,19 +30,19 @@ class CreateAvatarsTable extends Migration
         Schema::create('avatars', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable();
-            $table->string('skin')->nullable();
-            $table->string('head')->nullable();
-            $table->string('faceshape')->nullable();
-            $table->string('facehair')->nullable();
-            $table->string('hair')->nullable();
-            $table->string('eyes')->nullable();
-            $table->string('eyebrows')->nullable();
-            $table->string('nose')->nullable();
-            $table->string('lips')->nullable();
-            $table->string('pe')->nullable();
-            $table->string('accesories')->nullable();
-            $table->string('outfit')->nullable();
-            $table->string('shoes')->nullable();
+            $table->string('skin')->default('NONE');
+            $table->string('head')->default('NONE');
+            $table->string('faceshape')->default('NONE');
+            $table->string('facehair')->default('NONE');
+            $table->string('hair')->default('NONE');
+            $table->string('eyes')->default('NONE');
+            $table->string('eyebrows')->default('NONE');
+            $table->string('nose')->default('NONE');
+            $table->string('lips')->default('NONE');
+            $table->string('pe')->default('NONE');
+            $table->string('accesories')->default('NONE');
+            $table->string('outfit')->default('NONE');
+            $table->string('shoes')->default('NONE');
             $table->timestamps();
         });
     }
