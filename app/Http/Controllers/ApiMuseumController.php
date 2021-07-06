63798a9b8ea31e67c07409db489e38af401bbfc7
unknown <williamgalas2@gmail.com>
2021-07-06 21:13:46 +0800

    genre

--- app/Http/Controllers/ApiMuseumController.php
+++ app/Http/Controllers/ApiMuseumController.php
@@ -8,10 +8,13 @@ use Illuminate\Http\Request;
 class ApiMuseumController extends Controller
 {
     public function index(){
+        request()->validate([
+            'genre'=>'required'
+        ]);
         if(request()->has('_limit')){
-            $arts = Art::limit(request()->_limit)->get();
+            $arts = Art::where('genre', request()->genre)->limit(request()->_limit)->get();
         }else {
-            $arts = Art::get();
+            $arts = Art::where('genre', request()->genre)->get();
         }
         return response([
             'art_scenes'=>$arts,
