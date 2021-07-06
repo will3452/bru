fa5dd9471bba2821dd2099aded4154d076329e00
unknown <williamgalas2@gmail.com>
2021-07-07 01:38:55 +0800

    fix museum

--- routes/api.php
+++ routes/api.php
@@ -42,8 +42,8 @@ Route::prefix('v1')->group(function(){
         // Route::get('/audio-books', ); 
         
         // museum
-        Route::get('/museum', 'ApiMuseumController@index');
         Route::get('/museum/{id}', 'ApiMuseumController@show');
+        Route::get('/museum', 'ApiMuseumController@index');
         Route::get('/museum-summary', 'ApiMuseumController@summary');
 
         // library
