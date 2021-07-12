454d561767c266a2758d3179152247566dc69dbd
unknown <williamgalas2@gmail.com>
2021-07-13 00:34:27 +0800

    fixed banner

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -1,41 +1,71 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
-    <title>Banner</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+      ul {
+        display: flex;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+      }
+      li {
+        display: block;
+        list-style: none;
+      }
+      button.prev,
+      button.next {
+        display: flex;
+        font-size: 32px;
+        align-items: center;
+        justify-content: center;
+        color: #fff;
+        background-color: #333;
+        border-radius: 50%;
+        width: 44px;
+        height: 44px;
+      }
+      button.prev {
+        position: absolute;
+        left: 0;
+        top: 80px;
+      }
+      button.next {
+        position: absolute;
+        right: 0;
+        top: 80px;
+      }
+      li img {
+        width: 100vw;
+        /* height: 100vh; */
+        object-fit: cover;
+      }
+    </style>
   </head>
   <body>
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-           @foreach (\App\Banner::get() as $banner)
-                <div class="carousel-item 
-                @if ($loop->first)
-                    active
-                @endif
-                ">
-                    <img src="{{ url($banner->image) }}" class="d-block w-100" alt="banner">
-                </div>
-           @endforeach
-           
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
+    <ul>
+      @foreach (\App\Banner::get() as $item)
+          <li>
+            <img src="{{ $item->image }}" alt="">
+          </li>
+      @endforeach
+    </ul>
+    <!-- <button onclick="show(-1)" class="prev">&lt;</button> -->
+    <!-- <button onclick="show(+1)" class="next">&gt;</button> -->
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-
+    <script>
+      let liEls = document.querySelectorAll("ul li");
+      let index = 0;
+      window.show = function (increase) {
+        index = index + increase;
+        index = Math.min(Math.max(index, 0), liEls.length - 1);
+        liEls[index].scrollIntoView({ behavior: "smooth" });
+      };
+      let counter = 0;
+      setInterval(show, 2000, ++counter);
+    </script>
   </body>
 </html>
