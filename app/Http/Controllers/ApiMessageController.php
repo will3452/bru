60c57b6b5e203b361245d9c5f8854476c66666b9
unknown <williamgalas2@gmail.com>
2021-08-05 01:07:08 +0800

    fix

--- app/Http/Controllers/ApiMessageController.php
+++ app/Http/Controllers/ApiMessageController.php
@@ -29,8 +29,8 @@ class ApiMessageController extends Controller
     {
         $user = User::find(auth()->user()->id);
 
-        $inbox = $user->inboxes()->latest()->get();
-        $outbox = $user->outboxes()->latest()->get();
+        $inbox = $user->inboxes()->with('sender')->latest()->get();
+        $outbox = $user->outboxes()->with('receiver')->latest()->get();
 
         return response([
             'inbox_len' => count($inbox),
@@ -44,8 +44,8 @@ class ApiMessageController extends Controller
     {
         $user = User::find(auth()->user()->id);
 
-        $inbox = $user->inboxes()->latest()->get();
-        $outbox = $user->outboxes()->latest()->get();
+        $inbox = $user->inboxes()->with('sender')->latest()->get();
+        $outbox = $user->outboxes()->with('receiver')->latest()->get();
 
         return response([
             'inbox_len' => count($inbox),
