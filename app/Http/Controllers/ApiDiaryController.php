833f973917a67ed32a6a3d935c17ad9fe8d46f15
williamgalas-cp <william.galas@coreproc.ph>
2021-08-16 01:24:09 +0800

    fixed

--- app/Http/Controllers/ApiDiaryController.php
+++ app/Http/Controllers/ApiDiaryController.php
@@ -75,7 +75,7 @@ class ApiDiaryController extends Controller
             $date = Carbon::parse($date);
         }
 
-        $user = User::find(auth()->user()->id ?? 1);
+        $user = User::find(auth()->user()->id ?? 6);
         $weeks = \DB::table('logs')
             ->where('user_id', $user->id)
             ->where('created_at', '>=', $startWeek)
