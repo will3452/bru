9da5a8ef108bfb3a63e97a0988503b2be847175b
William Galas <william.galas@coreproc.ph>
2021-08-18 22:36:05 +0800

    fix

--- app/Http/Controllers/ApiAvatarController.php
+++ app/Http/Controllers/ApiAvatarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Avatar;
+use App\User;
 use Illuminate\Http\Request;
 
 class ApiAvatarController extends Controller
@@ -49,6 +50,22 @@ class ApiAvatarController extends Controller
     public function show()
     {
         $avatar = Avatar::where('user_id', auth()->user()->id)->first();
+
+        $user = User::find(auth()->user()->id);
+
+        foreach ($avatar as $key => $value) {
+            $item = $user->box->products()->where('id', $value)->first();
+            if ($item) {
+                $avatar[$key] = [
+                    'image' => $item->picture,
+                    'x' => $item->x,
+                    'y' => $item->y,
+                    'width' => $item->width,
+                    'height' => $iten->height,
+                ];
+            }
+        }
+
         return response([
             'avatar' => $avatar ?? [],
             'result' => 200,
