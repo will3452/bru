f4ef80b61f68a070abbcd481b004c0e51c738665
William Galas <william.galas@coreproc.ph>
2021-08-19 03:22:31 +0800

    cache to server

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,93 +7,104 @@ use App\Book;
 use Illuminate\Http\Request;
 use App\Http\Requests\BookForms;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
-    public function __construct(){
+    public function __construct()
+    {
         $this->middleware('auth');
     }
-    public function index(){
+    public function index()
+    {
         return redirect()->route('home');
     }
 
-    public function store(BookForms $request){
+    public function store(BookForms $request)
+    {
         // dd($request->tag);
         $rtag = $request->tag;
-        
+
         $validated = $request->validated();
         $validated['cpy'] = now();
         unset($validated['tag']);
         $book = auth()->user()->books()->create($validated);
-        
 
         //attaching tags
-        if($request->has('tag')){
+        if ($request->has('tag')) {
             $tag_len = count($request->tag) < 10 ? count($request->tag) : 10;
-            for($i = 0; $i < $tag_len; $i++){
+            for ($i = 0; $i < $tag_len; $i++) {
                 $tags = Tag::where('name', $rtag[$i])->get();
-                if(!count($tags)){
-                    $tag = Tag::create(['name'=>$rtag[$i]]);
+                if (!count($tags)) {
+                    $tag = Tag::create(['name' => $rtag[$i]]);
                     $tag->books()->attach($book->id);
-                }else {
+                } else {
                     $tags[0]->books()->attach($book->id);
                 }
             }
         }
 
-        return redirect(route('books.list').'?id='.$book->id);
+        return redirect(route('books.list') . '?id=' . $book->id);
     }
-    
 
-    public function create(){
+    public function create()
+    {
         return view('books.create');
     }
 
-    public function list(){
-        if(request()->has('filter')){
-            if(request()->filter == 'all'){
-                $books = auth()->user()->books;
-            }else if(request()->filter == 'published'){
-                $books = auth()->user()->books()->whereNotNull('publish_date')->get();
-            }else if(request()->filter == 'not-yet'){
-                $books = auth()->user()->books()->whereNull('publish_date')->get();
-            }
-        }else {
-            $books = auth()->user()->books;
+    function list() {
+        if (request()->has('filter')) {
+            $books = Cache::remember('books' . auth()->user()->id, 15, function () {
+                if (request()->filter == 'all') {
+                    return auth()->user()->books;
+                } else if (request()->filter == 'published') {
+                    return auth()->user()->books()->whereNotNull('publish_date')->get();
+                } else if (request()->filter == 'not-yet') {
+                    return auth()->user()->books()->whereNull('publish_date')->get();
+                }
+            });
+
+        } else {
+            $books = Cache::remember('books-all' . auth()->user()->id, 15, function () {
+                return auth()->user()->books;
+            });
         }
 
         return view('books.list', compact('books'));
     }
 
-    public function show(Book $book){
+    public function show(Book $book)
+    {
         return view('books.show', compact('book'));
     }
 
-    public function update(Book $book){
+    public function update(Book $book)
+    {
         $validated = request()->validate([
-            'category'=>'',
-            'genre'=>'',
-            'type'=>'',
-            'language'=>'',
-            'lead_character'=>'',
-            'lead_college'=>'',
-            'blurb'=>'',
-            'review_question_1'=>'',
-            'review_question_2'=>'',
-            'credit_page'=>'',
-            'publish_date'=>''
+            'category' => '',
+            'genre' => '',
+            'type' => '',
+            'language' => '',
+            'lead_character' => '',
+            'lead_college' => '',
+            'blurb' => '',
+            'review_question_1' => '',
+            'review_question_2' => '',
+            'credit_page' => '',
+            'publish_date' => '',
         ]);
 
         $d = $book->update($validated);
         return redirect(route('home'))->withSuccess('Save Changed!');
     }
 
-    public function destroy(Book $book){
-        
-        if(!Hash::check(request()->password, auth()->user()->password)){
+    public function destroy(Book $book)
+    {
+
+        if (!Hash::check(request()->password, auth()->user()->password)) {
             return abort(401);
         } else {
-            if(request()->has('reason_delete')) {
+            if (request()->has('reason_delete')) {
                 $book->reason_delete = request()->reason_delete;
                 $book->save();
             }
@@ -102,16 +113,16 @@ class BookController extends Controller
         }
     }
 
-
-    public function updateFront(Book $book){
+    public function updateFront(Book $book)
+    {
         request()->validate([
-            'front'=>'required'
+            'front' => 'required',
         ]);
 
         $path = request()->front->store('public/fronts');
         $arr_path = explode('/', $path);
         $end_path = end($arr_path);
-        $front = '/storage/fronts/'.$end_path;
+        $front = '/storage/fronts/' . $end_path;
         $book->front = $front;
         $book->save();
         return redirect()->route('books.show', $book)->withSuccess('Done!');
