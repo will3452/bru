87081fc1e10e8b849108b4fca598f851cdab44f9
william galas <williamgalas2@gmail.com>
2021-08-21 03:47:15 +0800

    fix

--- routes/api.php
+++ routes/api.php
@@ -5,21 +5,6 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use Luigel\Paymongo\Facades\Paymongo;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
- */
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::prefix('v1')->group(function () {
 
     // proctected via sanctum
