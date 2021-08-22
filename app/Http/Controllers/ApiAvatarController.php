699eddd73bc7347fa79ba8bb5403a2c8f2323aca
william galas <williamgalas2@gmail.com>
2021-08-23 04:36:11 +0800

    fix

--- app/Http/Controllers/ApiAvatarController.php
+++ app/Http/Controllers/ApiAvatarController.php
@@ -49,15 +49,39 @@ class ApiAvatarController extends Controller
     public function show()
     {
         $avatar = Avatar::where('user_id', auth()->user()->id)->first();
-        return $avatar;
-        $user = User::find(auth()->user()->id);
-        $exclude = ['id', 'user_id'];
-        $keys = collect();
-        foreach ($avatar as $item) {
-            $keys->push($item);
+
+        $pe = Product::where('id', $avatar->pe)->first();
+        $outfit = Product::where('id', $avatar->outfit)->first();
+        $shoes = Product::where('id', $avatar->shoes)->first();
+        //if pe is dynamic
+        if ($pe) {
+            $avatar->pe = [
+                'image' => $pe->image,
+                'width' => $pe->width,
+                'height' => $pe->height,
+                'x' => $pe->x,
+                'y' => $pe->y,
+            ];
+        }
+        if ($outfit) {
+            $avatar->outfit = [
+                'image' => $outfit->image,
+                'width' => $outfit->width,
+                'height' => $outfit->height,
+                'x' => $outfit->x,
+                'y' => $outfit->y,
+            ];
+        }
+        if ($shoes) {
+            $avatar->shoes = [
+                'image' => $shoes->image,
+                'width' => $shoes->width,
+                'height' => $shoes->height,
+                'x' => $shoes->x,
+                'y' => $shoes->y,
+            ];
         }
 
-        return $avatar;
         return response([
             'avatar' => $avatar ?? [],
             'result' => 200,
