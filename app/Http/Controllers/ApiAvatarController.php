9b26c55e989feac66465bccdd2724edd8c34455c
william galas <williamgalas2@gmail.com>
2021-08-23 02:46:04 +0800

    fix

--- app/Http/Controllers/ApiAvatarController.php
+++ app/Http/Controllers/ApiAvatarController.php
@@ -49,6 +49,7 @@ class ApiAvatarController extends Controller
     public function show()
     {
         $avatar = Avatar::where('user_id', auth()->user()->id)->first();
+        return $avatar;
 
         $user = User::find(auth()->user()->id);
 
