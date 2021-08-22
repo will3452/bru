a1217ed5171fade2764c4f11e50537e8ae240217
william galas <williamgalas2@gmail.com>
2021-08-23 02:07:51 +0800

    fix

--- resources/views/arts/list.blade.php
+++ resources/views/arts/list.blade.php
@@ -4,7 +4,7 @@
     <a href="{{ route('books.index') }}" class="btn btn-primary btn-sm mb-2"><i class="fa fa-angle-left"></i> Back</a>
     <div class="d-flex mt-4 flex-wrap">
         @foreach ($arts as $book)
-            <x-book-window :book="$book"/>
+            <x-book-window :book="$book" link="{{ route('arts.show', $book) }}"/>
         @endforeach
     </div>
     
--- resources/views/components/book-window.blade.php
+++ resources/views/components/book-window.blade.php
@@ -1,10 +1,10 @@
-@props(['book'])
+@props(['book', 'link'])
 <div  class="m-4 text-center " style="position:relative;" x-data="{showInfo:false}" x-on:mouseover="showInfo = true" x-on:mouseout="showInfo = false">
             <div  id="{{ $book->id }}" style="cursor:pointer;@if(request()->id == $book->id)  box-shadow:0px 0px 10px 10px #1A0A49; @else box-shadow:10px 5px 2px #555 ; @endif height: 200px; width: 150px; background:url('{{ $book->cover??$book->file??$book->content }}');background-size:contain;background-position:center;">
 
             </div>
             <div class="mt-2" style="word-wrap: break-word;width: 150px;">
-                <a href="{{ route('books.show', $book) }}" style="text-transform:capitalize;">{{ $book->title}}</a>
+                <a href="{{ $link }}" style="text-transform:capitalize;">{{ $book->title}}</a>
             </div>
             <div  x-show.transition="showInfo" style="position:absolute;top:-10px;left:100px; height:300px; width:200px;background:rgba(17,0,31, 0.9);z-index:999" >
                 <div style="position:absolute;top:6px;left:-14px;height:32px; width:32px;border:4px #014E7F solid;background:url('{{ asset('img/card-bg-custom.png') }}');background-position:center;border-top:none;border-right:none;transform:rotate(45deg);z-index:998;"></div>
