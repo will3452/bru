85d7ebebf11b20d28c5753d254e140fc834da3f5
william galas <williamgalas2@gmail.com>
2021-08-26 05:18:55 +0800

    fix

--- resources/views/marketing/index.blade.php
+++ resources/views/marketing/index.blade.php
@@ -4,60 +4,62 @@
         <h1 class="h3 mb-4 text-gray-800">{{ __('Marketing') }}</h1>
         <a href="{{ route('marketing.create') }}" class="btn btn-primary btn-sm">Create New</a>
     </div>
-    <table id="marketable" class="table">
-        <thead>
-            <tr>
-                <th>
-                    ID
-                </th>
-                <th>
-                    Category
-                </th>
-                <td>
-                    Schedule
-                </td>
-                <td>
-                    Duration
-                </td>
-                <td>
-                    Cost
-                </td>
-                <td>
-                    Status
-                </td>
-                <td>
-                    Option
-                </td>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($markets as $market)
+    <x-card header="List">
+        <table id="marketable" class="table">
+            <thead>
                 <tr>
+                    <th>
+                        ID
+                    </th>
+                    <th>
+                        Category
+                    </th>
                     <td>
-                        {{ $market->unique_id }}
+                        Schedule
                     </td>
                     <td>
-                        {{ $market->string_category }}
+                        Duration
                     </td>
                     <td>
-                        {{ $market->schedule }}
+                        Cost
                     </td>
                     <td>
-                        {{ $market->day_duration }}
+                        Status
                     </td>
                     <td>
-                        {{ $market->cost }}
-                    </td>
-                    <td>
-                        {{ $market->status }}
-                    </td>
-                    <td>
-                        <a href="{{ route('marketing.show', $market->id) }}" class="btn btn-primary btn-sm">show</a>
+                        Option
                     </td>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($markets as $market)
+                    <tr>
+                        <td>
+                            {{ $market->unique_id }}
+                        </td>
+                        <td>
+                            {{ $market->string_category }}
+                        </td>
+                        <td>
+                            {{ $market->schedule }}
+                        </td>
+                        <td>
+                            {{ $market->day_duration }}
+                        </td>
+                        <td>
+                            {{ $market->cost }}
+                        </td>
+                        <td>
+                            {{ $market->status }}
+                        </td>
+                        <td>
+                            <a href="{{ route('marketing.show', $market->id) }}" class="btn btn-primary btn-sm">show</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </x-card>
 @endsection
 @section('top')
     <link rel="stylesheet" href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}">
