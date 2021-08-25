6c0bdaf24967289b816777b3bf7b18a3792cee44
william galas <williamgalas2@gmail.com>
2021-08-25 18:47:38 +0800

    fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -91,12 +91,19 @@
 
 
         <x-sidebar.navitem link="{{ route('marketing.index') }}" label="Marketing">
-           <i class="fa fa-store" style="font-size:24px;color:white"></i>
+           <i class="fa fa-bullhorn" style="font-size:24px;color:white"></i>
         </x-sidebar.navitem>
 
 
         <x-sidebar.navitem link="{{ route('profile') }}" label="Profile">
             <img src="{{ asset('img/icons/profile.png') }}" alt="" class="icon">
+        </x-sidebar.navitem>
+
+        <x-sidebar.navitem label="Logout">
+            <i class="fas fa-sign-out-alt"  style="font-size:24px;color:white" onclick="document.getElementById('logout-form').submit()"></i>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
         </x-sidebar.navitem>
 
 
@@ -222,21 +229,6 @@
                                 <img src="{{ auth()->user()->picture }}"  class="rounded-circle avatar avatar font-weight-bold" style="object-fit: cover" alt="">
                             @endif
                         </a>
-                        <!-- Dropdown - User Information -->
-                        <div class="dropdown-menu dropdown-menu-right bg-primary shadow animated--grow-in" aria-labelledby="userDropdown">
-                            {{-- <a class="dropdown-item" href="javascript:void(0)">
-                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                {{ __('Settings') }}
-                            </a> --}}
-                            {{-- <a class="dropdown-item" href="javascript:void(0)">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                {{ __('Activity Log') }}
-                            </a> --}}
-                            <a class="dropdown-item bg-primary" href="#" data-toggle="modal" data-target="#logoutModal">
-                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                {{ __('Logout') }}
-                            </a>
-                        </div>
                     </li>
 
                 </ul>
@@ -275,28 +267,6 @@
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
-
-<!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">{{ __('Ready to Leave?') }}</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-link" type="button" data-dismiss="modal">{{ __('Cancel') }}</button>
-                <a class="btn btn-danger" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 
 <!-- Scripts -->
 <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
