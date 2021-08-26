14d0ecbcd38acd17af5ca0d90b48fc3e5293fcf9
william galas <williamgalas2@gmail.com>
2021-08-27 04:31:48 +0800

    fix

--- resources/views/test-video.blade.php
+++ resources/views/test-video.blade.php
@@ -1,32 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello world</title>
-    <style>
-        html, body {
-            margin: 0px;
-            padding: 0px;
-            overflow: hidden;
-        }
-        
-    </style>
-    <link href="https://vjs.zencdn.net/7.14.3/video-js.css" rel="stylesheet" />
+  <link href="https://vjs.zencdn.net/7.14.3/video-js.css" rel="stylesheet" />
+
+  <!-- If you'd like to support IE8 (for Video.js versions prior to v7) -->
+  <!-- <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script> -->
 </head>
+
 <body>
-    <video
+  <video
     id="my-video"
-    class="video-js"
+    class="video-js vjs-big-play-centered"
     controls
     preload="auto"
-    style="width:100vw; height:100vh;" 
     poster="MY_VIDEO_POSTER.jpg"
     data-setup="{}"
   >
-    <source src="{{ $src }}" type="video/mp4" />
-    <source src="{{ $src }}" type="video/webm" />
+    <source src="{{  $src }}" type="video/mp4" />
+    <source src="{{ $src }}"type="video/webm" />
     <p class="vjs-no-js">
       To view this video please enable JavaScript, and consider upgrading to a
       web browser that
@@ -35,6 +24,17 @@
       >
     </p>
   </video>
+
   <script src="https://vjs.zencdn.net/7.14.3/video.min.js"></script>
+
+  <script>
+    
+    window.onload = function(){
+
+      let vd = videojs('my-video');
+      vd.responsive(true);
+
+    }
+
+  </script>
 </body>
-</html>
