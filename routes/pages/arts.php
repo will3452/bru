fe5ea543bc806f8ae2aa708730bf3219eb576cf5
william galas <williamgalas2@gmail.com>
2021-08-28 10:20:36 +0800

    fix

--- routes/pages/arts.php
+++ routes/pages/arts.php
@@ -2,7 +2,7 @@
 
 Route::prefix('arts')->name('arts.')->middleware(['auth'])->group(function () {
     Route::get('/create', 'ArtSceneController@create')->name('create');
-    Route::get('/list', 'ArtSceneController@list')->name('list');
+    Route::get('/', 'ArtSceneController@list')->name('list');
     Route::post('/', 'ArtSceneController@store')->name('store');
     Route::get('/{art}', 'ArtSceneController@show')->name('show');
     Route::put('/{art}', 'ArtSceneController@update')->name('update');
