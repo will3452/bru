c0af6284dc46b6e6e3c66c8c10a323bc8f0cfe96
william galas <williamgalas2@gmail.com>
2021-08-29 00:55:21 +0800

    fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,12 +12,11 @@
 
     <title>{{ config('app.name', 'BRUMUTLIVERSE') }}</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <x-vendor.fontawesome/>
 
     <!-- Styles -->
     <link href="{{ asset('css/sb-admin-2.css') }}" rel="stylesheet">
+    
     @livewireStyles
     @yield('top')
     <!-- Favicon -->
--- resources/views/test_gem.blade.php
+++ resources/views/test_gem.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <form action="/test" method="POST">
+<form action="/test" method="POST" style="border:2px solid #222">
+        <h1>ADD CRYSTAL</h1>
         @csrf
         <div>
             Enter Email
@@ -26,5 +18,3 @@
     @if (session('success'))
         Added!
     @endif
-</body>
-</html>
