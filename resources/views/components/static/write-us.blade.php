15ebaa225de775af4eaf22c857c153c68225323a
william galas <williamgalas2@gmail.com>
2021-09-03 02:17:52 +0800

    fix

---
+++ resources/views/components/static/write-us.blade.php
@@ -0,0 +1,1 @@
+<img src="/static/writewithus.png" alt="" id="writewithus"/>
--- resources/views/static/contact.blade.php
+++ resources/views/static/contact.blade.php
@@ -1,4 +1,27 @@
 <x-static.html>
     <x-static.banner/>
-    
+    <x-static.section>
+        <x-static.write-us/>
+        <p>
+            BRUMULTIVERSE is always in need of authors and artists, who see the world and the universe differently. We need you to help us inspire people to live their lives the way they are always meant to live it! We need you to help us awaken that spark of love, light and life within them through your stories and your artworks.             
+        </p>
+    </x-static.section>
+    <x-static.image-stick>
+        /static/contact.jpg
+    </x-static.image-stick>
+    <x-static.section>
+         <p>
+             Help us make a difference by touching hearts and souls. 
+         </p>
+    </x-static.section>
+    <x-static.section>
+        <p>
+            For interested authors and artists, please send sample materials – short stories or first three chapters of a manuscript you’re working on or your visual works of art – to submissions@brumultiverse.com, along with a brief introduction about you and your artistic life. We will get back to you as soon as we can.
+        </p>
+    </x-static.section>
+    <x-static.section>
+        <p>
+            Please note, however, that while we recognize your awesome works and ideas, we only publish stories set within the BRUMULTIVERSE. 
+        </p>
+    </x-static.section>
 </x-static.html>
