b5d8246194f98eb9a5d5bfa98f954b6e15c8b1bc
william galas <williamgalas2@gmail.com>
2021-09-03 01:29:51 +0800

    fix

--- app/Http/Controllers/ApiNotificationController.php
+++ app/Http/Controllers/ApiNotificationController.php
@@ -9,25 +9,25 @@ class ApiNotificationController extends Controller
         $notifications = collect([
             [
                 'type' => 'announcement',
-                'message' => 'lorem ipsum dolor set',
+                'message' => 'lorem ipsum dolor set 1',
                 'more' => '',
                 'date' => now()->diffForHumans(),
             ],
             [
                 'type' => 'announcement 2',
-                'message' => 'lorem ipsum dolor set',
+                'message' => 'lorem ipsum dolor set 2',
                 'more' => '',
                 'date' => now()->diffForHumans(),
             ],
             [
                 'type' => 'announcemen 3',
-                'message' => 'lorem ipsum dolor set',
+                'message' => 'lorem ipsum dolor set 3',
                 'more' => '',
                 'date' => now()->diffForHumans(),
             ],
             [
                 'type' => 'announcemen 4',
-                'message' => 'lorem ipsum dolor set',
+                'message' => 'lorem ipsum dolor set 4',
                 'more' => '',
                 'date' => now()->diffForHumans(),
             ],
