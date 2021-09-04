c9ecf33dab41b0426237efd792e1daa6bc430e54
william galas <williamgalas2@gmail.com>
2021-09-05 05:31:23 +0800

    fix

--- nova/resources/views/auth/login.blade.php
+++ nova/resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 @include('nova::auth.partials.header')
 
 <form
-    class="shadow rounded-lg p-8 max-w-login mx-auto bg-indigo-900"
+    class="bg-indigo-900 shadow rounded-lg p-8 max-w-login mx-auto "
     method="POST"
     action="{{ route('nova.login') }}"
 >
