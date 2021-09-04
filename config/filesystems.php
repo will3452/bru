fec4911e3614d3ad03d30581e9b4be2ae508880f
william galas <williamgalas2@gmail.com>
2021-09-05 05:13:28 +0800

    fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -11,7 +11,7 @@ return [
     | by the framework. The "local" disk, as well as a variety of cloud
     | based disks are available to your application. Just store away!
     |
-    */
+     */
 
     'default' => env('FILESYSTEM_DRIVER', 'local'),
 
@@ -24,7 +24,7 @@ return [
     | reason, you may specify a default "cloud" driver here. This driver
     | will be bound as the Cloud disk implementation in the container.
     |
-    */
+     */
 
     'cloud' => env('FILESYSTEM_CLOUD', 's3'),
 
@@ -39,7 +39,7 @@ return [
     |
     | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
-    */
+     */
 
     'disks' => [
 
@@ -51,17 +51,16 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
         'nova' => [
-            'driver' => 'nova',
+            'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/',
+            'url' => env('APP_URL') . '/',
             'visibility' => 'public',
         ],
-
 
         's3' => [
             'driver' => 's3',
@@ -83,7 +82,7 @@ return [
     | `storage:link` Artisan command is executed. The array keys should be
     | the locations of the links and the values should be their targets.
     |
-    */
+     */
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
