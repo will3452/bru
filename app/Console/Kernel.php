7091d1c4e418202a6268e3c8b7eba588ffcb4e00
william galas <williamgalas2@gmail.com>
2021-09-12 17:18:55 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command(DailyHallPasses::class)
-            ->everyMinute();
+            ->dailyAt('6:00');
     }
 
     /**
