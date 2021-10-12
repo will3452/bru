c47b4e48aba66914542d84d216fadceb2276c0c4
will3452 <36730333+will3452@users.noreply.github.com>
2021-10-12 18:42:20 +0800

    Update auth.php

--- routes/pages/auth.php
+++ routes/pages/auth.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
-Auth::routes([]);
+Auth::routes(['login'=>false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/profile', 'ProfileController@index')->name('profile');
