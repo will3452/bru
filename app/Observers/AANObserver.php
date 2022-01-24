7f44a10075375038e6fc3d844985fadf6eaeb9db
William Galas <williamgalas2@gmail.com>
2022-01-24 21:05:58 +0800

    wip

--- app/Observers/AANObserver.php
+++ app/Observers/AANObserver.php
@@ -10,6 +10,6 @@ class AANObserver
     {
 
         $aan->admin_id = auth()->id();
-        $aan->complete = now()->format('ydm') . str_pad($aan->id, 8, '0', STR_PAD_LEFT);
+        $aan->complete = now()->format('ydm') . str_pad(AAN::count(), 8, '0', STR_PAD_LEFT);
     }
 }
